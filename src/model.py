a88e1c0e73aac2de4c979dc7268f491d168e46d9
Artem Sysuev <asysuev@gmail.com>
2018-10-21 15:24:25 +0300

    init commit

---
+++ src/model.py
@@ -0,0 +1,15 @@
+class Model():
+    def __init__():
+        pass
+    
+    def train():
+        pass
+    
+    def predict():
+        pass
+
+class ClassificationModel(Model):
+    pass
+
+class RegressionModel(Model):
+    pass
---
+++ train.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train-csv', type=argparse.FileType('r'), required=True)
+    parser.add_argument('--model-dir', required=True)
+    parser.add_argument('--mode', choices=['classification', 'regression'], required=True)
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
